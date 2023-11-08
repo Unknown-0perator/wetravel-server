1aa3511706463bd44dfa41c1948809bb4b8205a2
Ahmad Rashid Akhtar <ahmadrashidakhtar@gmail.com>
2023-11-07 16:50:19 -0800

    questionnaire table created

--- migrations/20231108003738_create_users_table.js
+++ migrations/20231108003738_create_users_table.js
@@ -8,3 +8,7 @@ exports.up = function (knex) {
             table.timestamp('created_at').defaultTo(knex.fn.now());
         })
 }
+
+exports.down = function (knex) {
+    return knex.schema.dropTable('users');
+}
