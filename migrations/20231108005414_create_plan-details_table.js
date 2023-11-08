798fcbf34820f24765e547681e87d838559f0538
Ahmad Rashid Akhtar <ahmadrashidakhtar@gmail.com>
2023-11-07 16:58:33 -0800

    plan_details table created

--- migrations/20231108005414_create_plan-details_table.js
+++ migrations/20231108005414_create_plan-details_table.js
@@ -1,15 +1,15 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function(knex) {
-  
-};
+exports.up = function (knex) {
+    return knex.schema
+        .createTable('plan_details', (table) => {
+            table.uuid('activity_id').primary();
+            table.uuid('plan_id').reference('plans.plan_id').onUpdate('CASCADE').onDelete('CASCADE').notNullable();
+            table.string('date').notNullable();
+            table.string('time').notNullable();
+            table.string('activity').notNullable();
+            table.timestamp('created_at').defaultTo(knex.fn.now());
+        })
+}
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
-  
-};
+exports.down = function (knex) {
+    return knex.schema.dropTable('plan_details');
+}
