e7e38848576e013310d5a263f0f05b7a4c256357
miguelmalo12 <miguelmalo12@gmail.com>
2023-11-13 19:56:10 -0800

    Added get '/' welcome and post 'logout' endpoints, and added port connection line for db connection

--- knexfile.js
+++ knexfile.js
@@ -7,5 +7,10 @@ module.exports = {
     database: process.env.DB_DNAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
+  },
+  migrations: {
+    tableName: 'knex_migrations',
+    directory: './migrations'
   }
 }
