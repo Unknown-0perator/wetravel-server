84c30ba5f71bb9679200f4be465b26f8cbb96881
Ahmad Rashid Akhtar <ahmadrashidakhtar@gmail.com>
2023-12-28 22:51:30 -0800

    Recommend me another destination functionality added

--- routes/recommend.js
+++ routes/recommend.js
@@ -47,7 +47,7 @@ router.post('/', async (req, res) => {
 
         res.status(200).json(resultText);
     } catch (error) {
-        res.status(500).json({ error: "Internal Server Error" });
+        res.status(500).json({ error: error.message });
     }
 });
 
